d516c5d690a274f3ebdb70a95953ae8187d69b28
Noëlie Ahuir <noelie.ahuir@edu.univ-fcomte.fr>
2024-11-20 11:03:51 +0100

    : défilement de base précédent/suivant

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,24 @@
 // *** CAROUSEL ***
 // TODO
+const carousel = document.querySelector(".carousel__container");
+const prevButton = document.querySelector(".carousel__button--prev");
+const nextButton = document.querySelector(".carousel__button--next");
+
+// Largeur de défilement d'un item
+if (carousel) {
+  // Scroll au clic sur le bouton précédent
+  prevButton.addEventListener("click", () => {
+    carousel.scrollBy( {
+        left : -260,
+        behavior: "smooth"
+    } );
+  });
+
+  // Scroll au clic sur le bouton suivant
+  nextButton.addEventListener("click", () => {
+    carousel.scrollBy({
+        left : +260,
+        behavior: "smooth"
+    } );
+  });
+}
