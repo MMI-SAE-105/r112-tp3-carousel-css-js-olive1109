eb17090d012ff17b1ebf4f0501c6462c45132ada
Noëlie Ahuir <noelie.ahuir@edu.univ-fcomte.fr>
2024-11-20 11:14:05 +0100

    défilement en fonction de la largeur d'une image

--- src/js/script.js
+++ src/js/script.js
@@ -3,13 +3,15 @@
 const carousel = document.querySelector(".carousel__container");
 const prevButton = document.querySelector(".carousel__button--prev");
 const nextButton = document.querySelector(".carousel__button--next");
+const premierItem = document.querySelector(".carousel__item");
+const scrollAmount = premierItem.clientWidth;
 
 // Largeur de défilement d'un item
 if (carousel) {
   // Scroll au clic sur le bouton précédent
   prevButton.addEventListener("click", () => {
     carousel.scrollBy( {
-        left : -260,
+        left : -scrollAmount,
         behavior: "smooth"
     } );
   });
@@ -17,7 +19,7 @@ if (carousel) {
   // Scroll au clic sur le bouton suivant
   nextButton.addEventListener("click", () => {
     carousel.scrollBy({
-        left : +260,
+        left : +scrollAmount,
         behavior: "smooth"
     } );
   });
